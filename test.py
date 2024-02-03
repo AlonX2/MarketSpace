ca903f0f1a3dc795ed31152cd74b81c9a7f3a3bd
Alon <alonat9@gmail.com>
2024-02-03 19:07:31 +0200

    test file removed

--- test.py
+++
@@ -1,7 +0,0 @@
-from vectorspace_inserter import VectorspaceInserter, OpenaiPineconeDriver
-from product.product import Product
-
-import logging
-logging.log
-prod = Product("uuid", "prod-name", "google.com", {"purpose": "To help people find content"})
-VectorspaceInserter(driver=OpenaiPineconeDriver()).insert_products([prod])
