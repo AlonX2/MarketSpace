3f004327126e728fbb504cc01902f89906c554eb
Alon <alonat9@gmail.com>
2024-02-03 15:49:33 +0200

    semi written vectorspace driver

---
+++ vectorspace_driver/openai_pinecone_driver.py/driver.py
@@ -0,0 +1,27 @@
+from pinecone import Pinecone
+from openai import OpenAI
+import numpy as np
+
+PINECONE_API_KEY = "e4d3c997-d68d-4f03-a42a-6c60c6dd9b02"
+OPENAI_API_KEY = "[YOUR KEY HERE]"
+PINECONE_INDEX_NAME = "test"
+RES_FILE = "out.txt"
+
+class OpenaiPineconeDriver():
+    def __init__(self):
+        self.openai_client = OpenAI(api_key=OPENAI_API_KEY)
+        self.pinecone_index = Pinecone(api_key=PINECONE_API_KEY).Index(PINECONE_INDEX_NAME)
+
+    def _get_embedding(self, text, model="text-embedding-3-small"):
+        return np.array(
+            self.openai_client.embeddings.create(input=[text], model=model).data[0].embedding
+        )
+    
+    def insert(self, vectors, vectorspace):
+        self.pinecone_index.upsert(vectors=vectors, namespace=vectorspace)
+
+    def preprocess_vector(self, text, id, metadata=None):
+        vector = {"id": id, "values": self._get_embedding(text)}
+        if metadata is not None:
+            vector["metadata"] = metadata
+        return vector
