c3bd2195512a865ff3abec0bb8f04777dba40f09
talo58948 <talo58948@gmail.com>
2024-06-22 00:50:35 +0300

    made microservice_client depedent on rabbit client

--- backend/src/app/microservice_client/microservice_client.py
+++ backend/src/app/microservice_client/microservice_client.py
@@ -1,7 +1,6 @@
-import pika, pika.exceptions, uuid, logging
+import uuid, logging
 
-from typing import Callable
-
+from utils.rabbit import RabbitClient
 from utils.call_future import CallFuture
 from ._config import *
 from .exceptions import *
@@ -16,7 +15,7 @@ class MicroserviceClient():
             cls.instance = super(MicroserviceClient, cls).__new__(cls)
         return cls.instance
     
-    def __init__(self) -> None:
+    def __init__(self, rabbit_client: RabbitClient) -> None:
         """Initializes the `MicroserviceClient` class.
 
         Initializes the connection and the callback queue of the client.
@@ -24,44 +23,10 @@ class MicroserviceClient():
         :raises MicroserviceClientFailedRabbitConnection: Raised in case of failure to connect to RabbitMQ server.
         """
         self._call_record: dict[uuid.UUID, CallFuture] = {}
-        try:
-            connection_params = pika.ConnectionParameters(RABBITMQ_SERVER)
-            self._connection = pika.BlockingConnection(connection_params)
-            logger.info("Established connection to RabbitMQ server")
-        except pika.exceptions.AMQPConnectionError as e:
-            logger.error("Failed to connect to RabbitMQ server")
-            raise MicroserviceClientFailedRabbitConnection("Failed to connect to RabbitMQ server") from e
-        
-        self._channel = self._connection.channel()
-        logger.info("Created channel to RabbitMQ exchange server")
-
-        self._res_queue = self._create_exclusive_queue(message_callback=self._on_res)
-
+        self._rabbit_client = rabbit_client
+        self._res_queue = rabbit_client.create_exclusive_queue(message_callback=self._on_res)
+        rabbit_client.async_consume(self._res_queue, self._on_res)
         logger.info("Instance created")
-    
-    def __del__(self):
-        """Closes the connection to RabbitMQ server upon object destruction
-        """
-        connection = getattr(self, "_connection", None)
-        if connection is not None:
-            connection.close()
-
-    def _create_exclusive_queue(self, message_callback: Callable) -> str:
-        """Creates exclusive queue for this client
-
-        :param message_callback: The callback to be called upon getting messages from the newly created queue.
-        :return: The created queue name
-        """
-        result = self._channel.queue_declare(queue='', exclusive=True)
-        _queue_name = result.method.queue
-
-        logger.info(f"Callback queue created: {_queue_name}")
-
-        self._channel.basic_consume(
-            queue=_queue_name,
-            on_message_callback=message_callback,
-            auto_ack=True)
-        return _queue_name
 
     def _on_res(self, ch, method, props, body) -> None:
         """The callback function that handles responses from the different microservices, 
@@ -100,18 +65,12 @@ class MicroserviceClient():
         _call_future = CallFuture()
         self._call_record[corr_id] = _call_future
         
-        try:
-            self._channel.basic_publish(
-                exchange="",
-                routing_key=target_queue,
-                properties=pika.BasicProperties(
-                    reply_to=self._res_queue,
-                    correlation_id=corr_id,
-                ),
-                body=data_json)
-        except (pika.exceptions.NackError, pika.exceptions.UnroutableError) as e:
-            logger.error("Couldn\'t publish a message to RabbitMQ exchange")
-            raise MicroserviceClientException("RabbitMQ error") from e
+        self._rabbit_client.publish(
+            exchange="",
+            routing_key=target_queue,
+            content=data_json,
+            reply_to=self._res_queue,
+            correlation_id=corr_id)
         
         _call_future.happened.wait()
         if _call_future.error is not None:
