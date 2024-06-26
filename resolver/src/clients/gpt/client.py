99c3f8f564676314de23a2e1f818ff28bfa8aaca
talo58948 <talo58948@gmail.com>
2024-06-26 17:40:13 +0300

    added temperature control to gpt client, also formatted becuase it does so automatically

--- resolver/src/clients/gpt/client.py
+++ resolver/src/clients/gpt/client.py
@@ -1,4 +1,4 @@
-import logging, json
+import logging
 
 from openai import OpenAI
 
@@ -6,17 +6,20 @@ from ..interface import ILLMClient
 
 logger = logging.getLogger(__package__)
 
+
 class GPTClient(ILLMClient):
     """GPT client, implementation of the ILLMClient interface.
     """
+
     def __init__(self, api_key: str) -> None:
         try:
             self._client = OpenAI(api_key=api_key)
         except:
-            logger.error("Could not instansiate new OpenAI client, check OPENAI_API_KEY")
+            logger.error(
+                "Could not instansiate new OpenAI client, check OPENAI_API_KEY")
             raise
-    
-    def get_response(self, prompt) -> str:
+
+    def get_response(self, prompt, temperature=0.2) -> str:
         """Gets the response from the GPT instance for the prompt given to it.
 
         :param prompt: Prompt for the GPT instance.
@@ -25,12 +28,14 @@ class GPTClient(ILLMClient):
         try:
             res = self._client.chat.completions.create(
                 model="gpt-3.5-turbo-0125",
-                response_format={ "type": "json_object" },
+                response_format={"type": "json_object"},
                 messages=[
-                        {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
-                        {"role": "user", "content": prompt}
-                    ]
-                ).choices[0].message.content
+                    {"role": "system",
+                        "content": "You are a helpful assistant designed to output JSON."},
+                    {"role": "user", "content": prompt}
+                ],
+                temperature=temperature,
+            ).choices[0].message.content
             return res
         except:
             logger.error("Could not get response from GPT client.")
