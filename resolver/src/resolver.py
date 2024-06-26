2f6d16ada04d700c741a7d8f5a5ad792ce1ed605
talo58948 <talo58948@gmail.com>
2024-06-26 17:39:08 +0300

    Changed resolvers to be operational, also formatted because it does so automatically

--- resolver/src/resolver.py
+++ resolver/src/resolver.py
@@ -7,18 +7,20 @@ from src._templates import *
 
 logger = logging.getLogger(__package__)
 
+
 class Resolver():
     """
     A class that resolves the features of the products given to it
     """
+
     def __init__(self, from_backend, rabbit_channel: RabbitChannel, llm_client: ILLMClient, output_exchange, output_routing_key) -> None:
         self.from_backend = from_backend
         self.rabbit_channel = rabbit_channel
         self.llm_client = llm_client
         self.output_exchange = output_exchange
         self.output_routing_key = output_routing_key
-        logger.info(f"Initialized Resolver with from_backend={self.from_backend}")
-    
+        logger.info(
+            f"Initialized Resolver with from_backend={self.from_backend}")
 
     def build_product_from_msg(self, props, msg):
         """
@@ -28,12 +30,15 @@ class Resolver():
         :param msg: The message content in JSON format.
         :return: An instance of the Product class.
         """
-        logger.debug(f"Building product from message: props={props}, msg={msg}")
-        routing_info = ProductRoutingInfo(backend_request=self.from_backend, corr_id=props.correlation_id, target_queue=props.reply_to)
+        logger.debug(
+            f"Building product from message: props={props}, msg={msg}")
+        routing_info = ProductRoutingInfo(
+            backend_request=self.from_backend, corr_id=props.correlation_id, target_queue=props.reply_to)
         product_info = json.loads(msg)
-        product = Product(uid=product_info["uid"], name=product_info["name"], url=product_info["url"], routing_info=routing_info)
+        product = Product(uid=product_info["uid"], name=product_info["name"],
+                          url=product_info["url"], routing_info=routing_info)
         return product
-    
+
     def send_to_vectorizer(self, product, props):
         """
         Sends the product information to a vectorizer via RabbitMQ.
@@ -41,13 +46,14 @@ class Resolver():
         :param product: An instance of the Product class.
         :param props: The properties of the message.
         """
-        logger.debug(f"Sending product to vectorizer: product={product}, props={props}")
+        logger.debug(
+            f"Sending product to vectorizer: product={product}, props={props}")
         product_json = json.dumps(dataclasses.asdict(product))
         self.rabbit_channel.publish(self.output_exchange,
-                                   self.output_routing_key,
-                                   product_json,
-                                   correlation_id=props.correlation_id,
-                                   reply_to=props.reply_to)
+                                    self.output_routing_key,
+                                    product_json,
+                                    correlation_id=props.correlation_id,
+                                    reply_to=props.reply_to)
         logger.info(f"Product sent to vectorizer: {product_json}")
 
     def load_templates_from_file(self):
@@ -56,15 +62,16 @@ class Resolver():
     def resolve_from_template(self, template: str, **kwargs):
         prompt = template.format(**kwargs)
         res = self.llm_client.get_response(prompt)
-        return json.loads(res)["output"]
-    
+        return json.loads(res)
+
     def resolve_company_products(self, company_url: str):
         """
         Resolves the products of a given company using the LLM client.
         """
-        products_desc_list = self.resolve_from_template(COMPANY_PRODUCTS_PROMPT, company_url=company_url)
+        products_desc_list = self.resolve_from_template(
+            COMPANY_PRODUCTS_PROMPT, company_url=company_url)["output"]
         logger.warning(products_desc_list)
-        # return [Product(uuid.uuid4(), name=product["product_name"], url=product["product_url"]) for product in products_desc_list]
+        return [Product(str(uuid.uuid4()), name=product["product_name"], url=product["product_url"]) for product in products_desc_list]
 
     def resolve_product_features(self, product_name: str, product_url: str):
         """
@@ -72,9 +79,10 @@ class Resolver():
         """
         product_features = dict()
         for feature_name, feature_template in FEATURES_PROMPTS.items():
-            product_features[feature_name] = self.resolve_from_template(feature_template, product_name=product_name, product_url=product_url)
+            product_features[feature_name] = self.resolve_from_template(
+                feature_template, product_name=product_name, product_url=product_url)
         return product_features
-        
+
     def resolve_from_message(self, ch, method_frame, props, msg):
         """
         Resolves product information from a received message and processes it.
@@ -87,7 +95,8 @@ class Resolver():
             logger.debug(f"Resolving company products for URL: {company_url}")
             products = self.resolve_company_products(company_url)
 
-        # for product in products:
-        #     logger.debug(f"Resolving features for product: {product}")
-        #     product.features = self.resolve_product_features(product.name, product.url)
-        #     self.send_to_vectorizer(product, props)
+        for product in products:
+            logger.debug(f"Resolving features for product: {product}")
+            product.features = self.resolve_product_features(
+                product.name, product.url)
+            self.send_to_vectorizer(product, props)
