e2c02243565a5f17c18f9e0a59cbdaeeb813f725
talo58948 <talo58948@gmail.com>
2024-07-01 01:09:15 +0300

    Changed the resolver templates and parsing

--- resolver/src/_templates.py
+++ resolver/src/_templates.py
@@ -1,9 +1,18 @@
 FEATURES_PROMPTS = {
-    "problem": "Given the product description in this url {product_url}, Describe the problem the product \"{product_name}\" addresses",
-    "market": "Given the product description in this url {product_url}, Describe the market of the product \"{product_name}\""
+    "problem": """Given the product description in this url {product_url}, Describe the problem the product "{product_name}" addresses.
+output in a single JSON in the following manner only!:
+{{
+    "output": <problem description>
+}}""",
+    "market": """Given the product description in this url {product_url}, Describe the market of the product "{product_name}".
+output in a single JSON in the following manner only!:
+{{
+    "output": <market description>
+}}
+"""
 }
 
-COMPANY_PRODUCTS_PROMPT = """list all products by \"{company_url}\"
+COMPANY_PRODUCTS_PROMPT = """list all products by "{company_url}"
 output ONLY actual distinct products by the company, do not list aspects of a single product! if company has only one product, list it and it alone
 output in a single JSON in the following manner only!:
 {{
