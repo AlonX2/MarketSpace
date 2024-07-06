a961c048583046e38f9d48c53fd80ebc51cf887e
AlonX2 <73618411+AlonX2@users.noreply.github.com>
2024-07-06 22:17:54 +0300

    bugfixes (#39)

--- collector/crunchbase/src/run_collector.py
+++ collector/crunchbase/src/run_collector.py
@@ -8,7 +8,7 @@ from utils.rabbit import RabbitChannel
 
 logger = logging.getLogger("src")
 
-def get_orgs_by_categories(categories: list[str], start_date: str = None, end_date: str = None) -> list[dict]:
+def get_orgs_by_categories(categories: list[str], start_date: str = None, end_date: str = None, limit = 10000) -> list[dict]:
     """
     Searches crunchbase for organizaions in categories, with an option to filter by crunchbase registration dates.
     :param categories: The categories to find orgs in.
@@ -30,26 +30,31 @@ def get_orgs_by_categories(categories: list[str], start_date: str = None, end_da
     if end_date is not None:
         query.add_filter("created_at", "lte", end_date)            
     
-    return query.execute()
+
+    return query.execute(results_limit=limit)
         
 def collect():
     """
     Starts collecting organizations from Crunchbase, categories and date limits dictated by env vars.
     Sends a message to rabbit queue for each org found.
     """
-    org_categories, rabbit_exchange, rabbit_rk = get_env_vars(["CATEGORIES",
+    org_categories, rabbit_exchange, rabbit_rk, limit = get_env_vars(["CATEGORIES",
                                                                "RABBIT_EXCHANGE",
-                                                               "RABBIT_ROUTING_KEY"], required=True)
+                                                               "RABBIT_ROUTING_KEY",
+                                                               "LIMIT"], required=True)
     start_date, end_date = get_env_vars(["START_DATE", "END_DATE"])
 
     logger.info("Starting to build query to get orgs.")
-    orgs =  get_orgs_by_categories(org_categories.split(","), start_date, end_date)
+    orgs =  get_orgs_by_categories(org_categories.split(","), start_date, end_date, int(limit))
 
     with RabbitChannel.get_default_channel() as rabbit_channel:
         for org_info in orgs:
-            org = CrunchbaseOrganization.create_from_org_fields(org_info["properties"])
-            logger.info(f"Sending org {repr(org)} to rabbit.")
-            rabbit_channel.publish(rabbit_exchange, rabbit_rk, org.json)
+            try:
+                org = CrunchbaseOrganization.create_from_org_fields(org_info["properties"], query_for_missing_info=False)
+                logger.info(f"Sending org {repr(org)} to rabbit.")
+                rabbit_channel.publish(rabbit_exchange, rabbit_rk, org.json)
+            except Exception as e:
+                logger.error(f"Failed to handle organization info: {org_info}, error: {e}")
 
 if __name__ == "__main__":
     collect()
