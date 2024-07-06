4342cae5871fbcb02b7b47b44b5b8f4c0d287c21
talo58948 <talo58948@gmail.com>
2024-07-06 13:44:47 +0300

    finally fixed

--- frontend/src/ProductDashboard/index.jsx
+++ frontend/src/ProductDashboard/index.jsx
@@ -2,24 +2,24 @@ import { useSearchParams } from "react-router-dom";
 import ProductDashboard from "./ProductDashboard";
 import useFetch from "./useFetch";
 
-export default function ProductDashboardWrapper(){
+export default function ProductDashboardWrapper() {
     const [searchParams, setSearchParams] = useSearchParams()
     const name = searchParams.get("name")
     const url = searchParams.get("url")
-    
+
     const fetchUrl = `http://127.0.0.1/products?` + new URLSearchParams({
         "name": name,
         "url": url,
     }).toString();
-    const { data, error, loading } = useFetch(fetchUrl, name && url);
-    
-    if (loading){
+    const { res, err, loading } = useFetch(fetchUrl, name && url);
+
+    if (loading) {
         return <div>LOADING...</div>
     }
-    if (error){
+    if (err) {
         return <div>ERROR</div>
     }
-    if (data){
+    if (res) {
         const data = {
             score: 55,
             neighbors: ["shit", "ass", "nigga"]
--- frontend/src/ProductDashboard/useFetch.jsx
+++ frontend/src/ProductDashboard/useFetch.jsx
@@ -1,31 +1,20 @@
 import { useEffect, useState } from "react";
 
-export default function useFetch(url, shouldFetch){
+export default function useFetch(url, shouldFetch) {
     const [res, setRes] = useState(null);
     const [err, setErr] = useState(null);
     const [loading, setLoading] = useState(true);
 
-    console.log("NIBBER")
     useEffect(() => {
-        if (!shouldFetch){
-            console.log("doesn't fetch")
+        if (!shouldFetch) {
             return;
         }
-        console.log("FETCHING")
         fetch(url)
-        .then(r => {
-            result = r.json();
-            console.log(`GOT THIS RESULT:\n\n${result}\n\n`)
-            setRes(r.json());
-        })
-        .catch(e => {
-            console.log("BITCH")
-            setErr(e);
-        })
-        .finally(() => {
-            setLoading(false);
-        })
+            .then(r => r.json())
+            .then(d => setRes(d))
+            .catch(e => setErr(e))
+            .finally(() => setLoading(false));
     }, [url, shouldFetch]);
-    
-    return {res, err, loading};
+
+    return { res, err, loading };
 }
