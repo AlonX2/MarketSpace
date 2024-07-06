5da67df8cf9a6f02a9eb3ea420d15156f83710c2
Tal <73689715+talo58948@users.noreply.github.com>
2024-07-06 22:48:41 +0300

    fixed frontend (#40)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,14 +8,14 @@ import './App.css'
 function App() {
   return (
     <BrowserRouter>
-      <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-tr from-white via-blue-200 to-white ">
+      <div className="min-h-screen min-w-screen flex items-center justify-center bg-gradient-to-tr from-white via-blue-200 to-white ">
         <Routes>
           <Route path="/" element={<ProductForm />} />
           <Route path="/product-dashboard" element={<ProductDashboardWrapper />} />
         </Routes>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
